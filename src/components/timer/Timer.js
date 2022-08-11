4244c34ab14a591729be492a709a37a08df4a678
Anastasia21O <virstanastya21@gmail.com>
2022-08-11 12:09:43 +0300

    (fix) create constructor to Timer

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -2,7 +2,18 @@ import React, {Component} from "react";
 
 
 class Timer extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
 
+    render() {
+        return (
+            <div></div>
+        );
+    }
 }
 
 export default Timer
